7c67720665e3d3df1c1930f9efe7d16219c42564
evoosa <eevvoossaa@gmail.com>
2020-07-31 18:13:41 +0300

    removed redundant func

--- web_crawler.py
+++ web_crawler.py
@@ -16,8 +16,8 @@ class WebCrawler(object):
     def __init__(self, root_url: str):
         self.root_url = root_url
         self.parsed_links = set()
-        self.report_path = 'C:\\Temp\\links_{}.txt'.format(NOW)
-        self.broken_links_report = 'C:\\Temp\\broken_links_{}.txt'.format(NOW)
+        self.report_path = '.\\links_{}.txt'.format(NOW)
+        self.broken_links_report = '.\\broken_links_{}.txt'.format(NOW)
 
     def get_links_from_url(self, url: str) -> set:
         """
@@ -34,17 +34,6 @@ class WebCrawler(object):
         bs_obj = BeautifulSoup(response.content, BS_PARSER, from_encoding=response.encoding)
         return {urljoin(url, link['href']) for link in bs_obj.find_all(href=True)}
 
-    def is_link_broken(self, url: str) -> bool:
-        """
-        Check whether a given link's URL is broken
-        :param url: URL to check if broken
-        :return: True if broken, False if not
-        """
-        response = requests.get(url)
-        if response.status_code == 404:
-            return True
-        return False
-
     def get_links_report(self, url: str, depth: int = 0) -> None:
         """
         Get a report of all links under a given website and their depth. check if they are broken
@@ -54,10 +43,10 @@ class WebCrawler(object):
         self.parsed_links.add(url)
         links_in_url = {link for link in self.get_links_from_url(url) if link.startswith(self.root_url)}
         for link in links_in_url:
-            with open(self.report_path, 'a', encoding='utf-8') as report_file:
-                report_line = "depth: {0}, link: {1}".format(depth, link)
-                report_file.write("\n" + report_line)
-                print(report_line)
             if link not in self.parsed_links:
+                with open(self.report_path, 'a', encoding='utf-8') as report_file:
+                    report_line = "depth: {0}, link: {1}".format(depth, link)
+                    report_file.write("\n" + report_line)
+                    print(report_line)
                 new_report_thread = threading.Thread(target=self.get_links_report, args=(link, depth + 1))
                 new_report_thread.start()
